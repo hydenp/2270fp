8ac4f112919f26efff71e7db59f50ec382476229
hydenpolikoff <hydenpolikoff@gmail.com>
2020-04-16 23:02:55 -0700

    initial file upload

---
+++ hash.hpp
@@ -0,0 +1,39 @@
+#ifndef HASH_HPP
+#define HASH_HPP
+
+#include <string>
+
+
+using namespace std;
+
+struct node
+{
+    int key;
+    struct node* next;
+};
+
+class HashTable
+{
+    int tableSize;  // No. of buckets (linked lists)
+
+    // Pointer to an array containing buckets
+    node* *table;
+    int numOfcolision = 0;
+    node* createNode(int key, node* next);
+    
+public:
+    HashTable(int bsize);  // Constructor
+
+    // inserts a key into hash table
+    bool insertItem(int key);
+
+    // hash function to map values to key
+    unsigned int hashFunction(int key);
+
+    void printTable();
+    int getNumOfCollision();
+
+    node* searchItem(int key);
+};
+
+#endif
